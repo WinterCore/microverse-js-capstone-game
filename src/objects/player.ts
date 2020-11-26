dc030f0ccaef0c5e4bf6630e713fa8cc41727660
WinterCore <hogobbl@gmail.com>
2020-11-26 14:12:09 +0200

    Add the ability to double jump

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -2,7 +2,12 @@ import * as Phaser from 'phaser';
 
 import { GameObjectArgs } from 'src/typings/index';
 
+const MAX_JUMPS = 2;
+const JUMP_FORCE = 200;
+
 class Player extends Phaser.Physics.Arcade.Sprite {
+    jumps: number = 2;
+
     constructor({ scene, x, y, texture, frame }: GameObjectArgs) {
         super(scene, x, y, texture, frame);
         this.setScale(0.4, 0.4);
@@ -10,12 +15,27 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.anims.play('PLAYER_RUN', true);
 
         scene.physics.world.enable(this);
-        this.scene.add.existing(this);
 
         this.setDepth(3);
         this.setGravityY(200);
+
+        this.scene.add.existing(this);
+
+        scene.input.keyboard.on('keydown_UP', this.jump, this);
+        scene.input.keyboard.on('keydown_SPACE', this.jump, this);
+        scene.input.on('pointerdown', this.jump, this);
     }
 
+    jump(): void {
+        if (this.body.touching.down) {
+            this.jumps = 0;
+        }
+
+        if (this.jumps < MAX_JUMPS) {
+            this.setVelocityY(JUMP_FORCE * -1);
+            this.jumps += 1;
+        }
+    }
 }
 
 export default Player;
--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,6 +1,7 @@
 import * as Phaser from 'phaser';
 
 import Player from '../objects/player';
+import Platform from '../objects/platform';
 
 class Game extends Phaser.Scene {
     private player?: Player;
@@ -10,9 +11,23 @@ class Game extends Phaser.Scene {
     }
 
     create(): void {
-        this.player = new Player({ scene: this, x: 100, y: 50, texture: 'character0' });
+        this.player = new Player({ scene: this, x: 100, y: 50, texture: 'character' });
+
+        const platform = new Platform({ scene: this, x: 100, y: 300, texture: 'grass' }, 3);
+
+        // @ts-ignore
+
+        this.physics.add.collider(this.player, platform, () => {
+            console.log('here');
+        }, undefined, this);
+        // this.add.existing(container1);
         // this.physics.add.sprite(10, 100, 'character0');
-        console.log(this.player);
+        // console.log(this.player);
+    }
+
+    update(): void {
+        const player = this.player!;
+        player.x = 100;
     }
 }
 
