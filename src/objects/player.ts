a06ead66433f3761a3c8d9f70e8a3dd716414670
WinterCore <hogobbl@gmail.com>
2020-11-26 02:13:52 +0200

    Add gravity, change depth & change scale

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -2,14 +2,20 @@ import * as Phaser from 'phaser';
 
 import { GameObjectArgs } from 'src/typings/index';
 
-class Player extends Phaser.GameObjects.Sprite {
+class Player extends Phaser.Physics.Arcade.Sprite {
     constructor({ scene, x, y, texture, frame }: GameObjectArgs) {
         super(scene, x, y, texture, frame);
-        this.setScale(0.5, 0.5);
+        this.setScale(0.4, 0.4);
         this.setOrigin(0, 0);
-        this.scene.add.existing(this);
         this.anims.play('PLAYER_RUN', true);
+
+        scene.physics.world.enable(this);
+        this.scene.add.existing(this);
+
+        this.setDepth(3);
+        this.setGravityY(200);
     }
+
 }
 
 export default Player;
