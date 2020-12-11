1abeb8e1d8b9bd80e2961a4ad9ec2cec3da0a966
WinterCore <hogobbl@gmail.com>
2020-12-11 13:33:36 +0200

    Fix eslint errors

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
-
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-typescript',
-    ],
-    plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties',
-    ],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    setupFiles: ['jest-canvas-mock']
+  setupFiles: ['jest-canvas-mock'],
 };
